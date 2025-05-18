4f45b8430032c887697fb54440de4d05e5dfb231
Gabrielalves5 <Gabrielalves5>
2025-05-18 20:59:18 -0300

    Fluxo

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Fluxograma da Engenharia");
+Console.WriteLine("A coisa está se movendo? [s] [n]");
+string escolha = Console.ReadLine()!;
+if (escolha == "s")
+{
+Console.WriteLine("Deveria?");
+escolha = Console.ReadLine()!;
+if (escolha == "s")
+{
+  Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine("Ótimo");
+    Console.ResetColor();
+}else
+{
+    Console.WriteLine("Usa Fita isolante");
+}
+}else if (escolha == "n")
+{
+  Console.WriteLine("Deveria?");
+escolha = Console.ReadLine()!;
+if (escolha == "s")
+Console.WriteLine("Usa WD-40");
+else{
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine("Ótimo");
+    Console.ResetColor();
+}
+}else
+return;
